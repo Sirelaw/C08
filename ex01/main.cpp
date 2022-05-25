fa96e4faf338fa976e0c24305e55e4c3a746f420
olawale Ipadeola <ipadeolaolawale+github@gmail.com>
2022-05-25 21:09:43 +0200

    edited ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,12 +9,12 @@ int	main()
 
 	for (int i = 0; i < test.size(); i++)
 		std::cout << test[i] << std::endl;
-	test.fill(test.begin(), test.begin() + 10);
-	for (int i = 0; i < 50; i++)
-		std::cout << test[i] << std::endl;
+	// test.fill(test.begin(), test.begin() + 10);
+	// for (int i = 0; i < 50; i++)
+	// 	std::cout << test[i] << std::endl;
 	test.fill();
-	for (int i = 0; i < 50; i++)
-		std::cout << test[i] << std::endl;
+	// for (int i = 0; i < 50; i++)
+	// 	std::cout << test[i] << std::endl;
 	// test.addNumber(20);
 	std::cout << "Longest span:\t" << test.longestSpan() << std::endl;
 	std::cout << "Shortest span:\t" << test.shortestSpan() << std::endl;
